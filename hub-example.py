4f3189b95a26e64c34944e693df2fcf0db750cf9
Sneyder Ospina Chavarria <sneyder.ospina@e-hps.com>
2025-12-04 16:00:00 -0500

    Added the code to consume an AI Hub

---
+++ hub-example.py
@@ -0,0 +1,35 @@
+import urllib.request
+import json
+
+# Request data goes here
+# The example below assumes JSON formatting which may be updated
+# depending on the format your endpoint expects.
+# More information can be found here:
+# https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+data = {"question": "What is there to do in San Francisco?",
+        'chat_history':[]}
+
+body = str.encode(json.dumps(data))
+
+url = ''
+# Replace this with the primary/secondary key, AMLToken, or Microsoft Entra ID token for the endpoint
+# apik = ''
+# if not apik:
+#     raise Exception("A key should be provided to invoke the endpoint")
+
+
+# headers = {'Content-Type':'application/json', 'Accept': 'application/json', 'Authorization':('Bearer '+ apik)}
+headers={}
+req = urllib.request.Request(url, body, headers)
+
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result)
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+    print(error.read().decode("utf8", 'ignore'))
